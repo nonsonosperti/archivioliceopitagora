e1059ee3b102d8088f5d55004fbce5e3bde10a56
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 00:34:31 +0200

    header test

--- header.php
+++ header.php
@@ -9,9 +9,99 @@
 <?php wp_body_open(); ?>
 <div id="wrapper" class="hfeed">
 <header>
-    <div>
-        
+<div id="pre-header" class="bg-petrol">
+    <div class="container">
+        <div class="row variable-gutters">
+            <div class="col-8">
+                <a href="https://www.miur.gov.it/" target="_blank" style="margin-right: 10px" aria-label="MIUR - Collegamento esterno - Apre su nuova scheda">
+                    <strong>MIUR</strong>
+                </a>
+                <a href="mailto:KRPC02000L@istruzione.it" style="margin-right: 10px" target="_blank" aria-label="Indirizzo email del liceo">
+                    <strong> <i class="fa fa-envelope" aria-hidden="true"></i> KRPC02000L@istruzione.it</strong>
+                </a>
+                <a href="tel:00390962905731" style="margin-right: 10px" target="_blank" aria-label="Numero di telefono del liceo">
+                    <strong> <i class="fa fa-phone" aria-hidden="true"></i> 0962905731</strong>
+                </a>
+                <span>Codice Meccanografico KRPC02000L  Codice Fatturazione UFHY7G</span>
+            </div><!-- /col-6 -->
+            <div class="col-4 header-utils-wrapper utils-moved">
+                <div class="header-utils">
+                        <div class="header-notification-alert has-notifications">
+        <a href="https://www.liceopitagoracrotone.edu.it/circolare/organizzazione-logistica-per-lo-svolgimento-degli-esami-di-stato-2024/" aria-label="Visualizza notifiche">
+            <svg class="svg-bell-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bell-solid"></use></svg>
+        </a>
     </div>
+    <!-- /header-notification-alert -->
+
+
+    <a class="toggle-user-menu-mobile toggle-menu menu-right push-body d-xl-none jPushMenuBtn" href="#">
+        <div class="avatar-wrapper">
+            <img src="https://secure.gravatar.com/avatar/15c527e8c7b8ad3bb6f2a89381fc85c6?s=250&amp;d=mm&amp;r=g" alt="Avatar utente">        </div><!-- /avatar-wrapper -->
+    </a>
+    <a class="dropdown-toggle d-none d-xl-flex" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" aria-label="dropdown toggle">
+        <div class="avatar-wrapper">
+            <img src="https://secure.gravatar.com/avatar/15c527e8c7b8ad3bb6f2a89381fc85c6?s=250&amp;d=mm&amp;r=g" alt="Avatar utente">        </div><!-- /avatar-wrapper -->
+        <p><strong>admin_liceopitagora</strong></p>
+    </a>
+    <div class="dropdown-menu dropdown-content menu-user menu-user-blue">
+        <div class="menu-user-wrapper">
+            <div class="user-details">
+                <div class="avatar-wrapper">
+                    <img src="https://secure.gravatar.com/avatar/15c527e8c7b8ad3bb6f2a89381fc85c6?s=250&amp;d=mm&amp;r=g" alt="Avatar utente">                </div><!-- /avatar-wrapper -->
+                <button type="button" aria-label="chiudi menu utente" class="close-user-menu jPushMenuBtn" tabindex="-1">
+                    <svg class="svg-cancel-large">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-cancel-large"></use>
+                    </svg>
+                </button>
+                <div class="user-details-content">
+                    <p><strong>admin_liceopitagora</strong></p>
+                    <p></p>
+                    <a class="btn btn-action btn-xs" href="https://www.liceopitagoracrotone.edu.it/wp-admin/" tabindex="-1">Crea e gestisci</a>
+                </div>
+            </div><!-- /user-details -->
+            <div class="menu-user-list">
+                <ul>
+                    <li class="active">
+                        <a href="https://www.liceopitagoracrotone.edu.it/wp-admin/">
+                            <span>Area personale</span>
+                            <svg class="svg-home-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-home-solid"></use></svg>
+                        </a>
+                    </li>
+                                            <li class="has-notifications">
+                            <a href="https://www.liceopitagoracrotone.edu.it/circolare/organizzazione-logistica-per-lo-svolgimento-degli-esami-di-stato-2024/">
+                                <span>Notifiche</span>
+                                <svg class="svg-bell-solid">
+                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bell-solid"></use>
+                                </svg>
+                            </a>
+                        </li>
+                                            <li>
+                        <a href="https://www.liceopitagoracrotone.edu.it/wp-admin/profile.php">
+                            <span>Modifica Profilo</span>
+                            <svg class="svg-bookmark-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bookmark-solid"></use></svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.liceopitagoracrotone.edu.it/author/admin_liceopitagora/">
+                            <span>Profilo Pubblico</span>
+                            <svg class="svg-user-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-user-solid"></use></svg>
+                        </a>
+                    </li>
+                </ul>
+            </div><!-- /menu-user-list -->
+            <div class="menu-user-bottom">
+                <a href="https://www.liceopitagoracrotone.edu.it/wp-login.php?action=logout&amp;_wpnonce=7ec7e59489">
+                    <span>Esci</span>
+                    <svg class="svg-exit"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-exit"></use></svg>
+                </a>
+            </div><!-- /menu-user-bottom -->
+        </div><!-- /menu-user-wrapper -->
+    </div><!-- /menu-user -->
+                </div><!-- /header-utils -->
+            </div><!-- /col-6 -->
+        </div><!-- /row -->
+    </div><!-- /container -->
+</div>
 </header>
 <div id="container">
 <main id="content" role="main">
