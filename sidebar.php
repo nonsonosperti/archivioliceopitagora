935541b09a0f034cc72f5250eccbf6968f8334c8
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 01:11:08 +0200

    no sidebar

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <aside id="sidebar" role="complementary">
 <div id="primary" class="widget-area">
 <ul class="xoxo">
-<?php dynamic_sidebar( 'primary-widget-area' ); ?>
+<?php //dynamic_sidebar( 'primary-widget-area' ); ?>
 </ul>
 </div>
 </aside>
