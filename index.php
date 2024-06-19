ae83620107f0dbb3e109fc402cb458baa9b42c12
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 16:31:20 +0200

    loop articoli

--- index.php
+++ index.php
@@ -4,13 +4,12 @@ get_header();
 // Query per raccogliere tutti i post da tutti i post types
 $args = array(
     'post_type' => 'any',  // Recupera da tutti i tipi di post
-    'posts_per_page' => -1, // Recupera tutti i post (senza limiti)
 );
 
-$query = new WP_Query( $args );
+$query = new WP_Query( array( 'post_type' => 'any' ) );
 
 
-if ( have_posts() ) : while ( have_posts() ) : the_post();
+if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
 get_template_part( 'entry' );
 comments_template();
 endwhile; endif;
