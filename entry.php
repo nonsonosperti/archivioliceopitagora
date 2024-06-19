0d959f59f8bf4bf2933fb19be8195d95c8a81d60
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 16:43:10 +0200

    post type

--- entry.php
+++ entry.php
@@ -1,8 +1,21 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header>
-<?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
+<?php 
+
+$post_type = get_post_type_object(get_post_type());
+
+if ( is_singular() ) { 
+    echo '<h1 class="entry-title" itemprop="headline">';
+    } else { echo '<h2 class="entry-title">'; } 
+    ?>
 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
-<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } 
+
+if ($post_type) {
+    echo ' <span class="post-type-label">' . esc_html($post_type->labels->singular_name) . '</span>';
+}
+
+?>
 <?php edit_post_link(); ?>
 <?php if ( !is_search() ) { get_template_part( 'entry', 'meta' ); } ?>
 </header>
