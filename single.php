5c1c893fa93497d8bf12f0ec759bb7482923639a
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 14:52:47 +0200

    test

--- single.php
+++ single.php
@@ -2,55 +2,17 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 <?php get_template_part( 'entry' ); ?>
 <?php
-
-$post_id = get_the_ID();
 // Ottieni tutti i metadati del post corrente
-$meta_fields = get_fields('options');
 
-echo '<pre>';
- echo $meta_fields;
-echo '</pre>';
 
-if ($meta_fields): ?>
-    <div class="custom-fields">
-        <h3>Custom Fields</h3>
-        <ul>
-            <?php foreach ($meta_fields as $key => $values): ?>
-                <?php 
-                // Ottieni l'oggetto campo utilizzando il nome del campo (key)
-                $field = get_field_object($key);
+$fields = get_fields();
 
-                if ($field):
-                    // La label del campo è fornita da ACF
-                    $label = $field['label'];
-
-                    // Ottieni il valore del campo
-                    $value = $field['value'];
-                ?>
-                    <li>
-                        <strong><?php echo esc_html($label); ?>:</strong>
-                        <?php
-                        if ($field['type'] === 'file' && !empty($value)) {
-                            // Se il campo è un file, visualizza come link
-                            $file_url = $value['url'];
-                            $file_name = $value['filename'];
-                            echo '<a href="' . esc_url($file_url) . '" target="_blank">' . esc_html($file_name) . '</a>';
-                        } else {
-                            // Per altri tipi di campi
-                            if (is_array($value)) {
-                                echo esc_html(implode(', ', $value));
-                            } else {
-                                echo esc_html($value);
-                            }
-                        }
-                        ?>
-                    </li>
-                <?php endif; ?>
-            <?php endforeach; ?>
-        </ul>
-    </div>
-<?php else: ?>
-    <p>No custom fields found.</p>
+if( $fields ): ?>
+    <ul>
+        <?php foreach( $fields as $name => $value ): ?>
+            <li><b><?php echo $name; ?></b> <?php echo $value; ?></li>
+        <?php endforeach; ?>
+    </ul>
 <?php endif; ?>
 
 <?php //if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
