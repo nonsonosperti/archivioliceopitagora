513e2fe16766c8b12129d4c68282609d2a3954ec
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 16:37:49 +0200

    loop

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $args = array(
     'posts_per_page' => -1,
 );
 
-$query = new WP_Query( array( 'post_type' => 'any' ) );
+$query = new WP_Query( $args );
 
 
 if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
