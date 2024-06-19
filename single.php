ab7bcb3df46d66f35f2269ee2c3cb7708c152c0d
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 14:42:37 +0200

    test loop

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
 <?php get_template_part( 'entry' ); ?>
 <?php
 
