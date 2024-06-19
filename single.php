6521cc3ffeed0f9bf043ec1bbb908e4c30b8ae1c
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-19 12:56:40 +0200

    altro test

--- single.php
+++ single.php
@@ -2,19 +2,39 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php
-// Ottieni tutti i custom fields del post corrente
-
-
-
-$fields = get_fields();
+// Ottieni tutti i campi personalizzati del post corrente
+$fields = get_field_objects();
 
 if( $fields ): ?>
-    <ul>
-        <?php foreach( $fields as $name => $value ): ?>
-            <li><b><?php echo $name; ?></b> <?php echo $value; ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="custom-fields">
+        <h3>Custom Fields</h3>
+        <ul>
+            <?php foreach( $fields as $field ): ?>
+                <li>
+                    <strong><?php echo esc_html($field['label']); ?>:</strong> 
+                    <?php 
+                    if ($field['type'] === 'file' && !empty($field['value'])) {
+                        // Se il campo è un file, visualizza come link
+                        $file_url = $field['value']['url'];
+                        $file_name = $field['value']['filename'];
+                        echo '<a href="' . esc_url($file_url) . '" target="_blank">' . esc_html($file_name) . '</a>';
+                    } else {
+                        // Per altri tipi di campi
+                        if (is_array($field['value'])) {
+                            echo esc_html(implode(', ', $field['value']));
+                        } else {
+                            echo esc_html($field['value']);
+                        }
+                    }
+                    ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php else: ?>
+    <p>No custom fields found.</p>
 <?php endif; ?>
+
 
 <?php //if ( comments_open() && !post_password_required() ) { comments_template( '', true ); } ?>
 <?php endwhile; endif; ?>
