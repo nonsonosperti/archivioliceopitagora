feb1e4a8b20611b2da126381baeb2c83dede9138
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-20 11:45:29 +0200

    test array

--- single.php
+++ single.php
@@ -11,47 +11,47 @@ echo '<pre>';
 echo var_dump($fields);
 echo '</pre>';
 
-if( $fields ): ?>
 
+if ($fields) : ?>
     <h3>Info e allegati</h3>
     <ul>
-        <?php foreach( $fields as $field ): ?>
-            <li><strong><?php echo $field['label']; ?>:</strong>
+        <?php foreach ($fields as $field) : ?>
+            <li>
+                <strong><?php echo $field['label']; ?>:</strong>
                 <?php
-
                 if (empty($field['value'])) {
                     echo 'Valore non disponibile';
                 } else {
-                // Verifica se il campo è un file
-                switch ($field['type']) {
-                    case 'file':
-                        $file_url = $field['value']['url'];
-                        echo '<a href="' . $file_url . '">'. $field['value']['filename'] .'</a>';
-                        break;
-                    case 'image':
-                        $image_url = $field['value']['url'];
-                        echo '<img src="' . $image_url . '" alt="' . $field['label'] . '" style="max-width:200px;"/>';
-                        break;
-                    case 'array':
-                        if (is_array($field['value'])) {
-                            echo '<ul>';
-                            foreach ($field['value'] as $item) {
-                                // Verifica se l'elemento dell'array è un'immagine
-                                if (isset($item['url'])) {
-                                    echo '<li><img src="' . $item['url'] . '" alt="' . $field['label'] . '" style="max-width:200px;"/></li>';
-                                } else {
-                                    echo '<li>' . $item . '</li>';
+                    // Gestione diversa in base al tipo di campo
+                    switch ($field['type']) {
+                        case 'file':
+                            $file_url = $field['value']['url'];
+                            echo '<a href="' . $file_url . '">'. $field['value']['filename'] .'</a>';
+                            break;
+                        case 'image':
+                            $image_url = $field['value']['url'];
+                            echo '<img src="' . $image_url . '" alt="' . $field['label'] . '" style="max-width:200px;"/>';
+                            break;
+                        case 'array':
+                            if (is_array($field['value'])) {
+                                echo '<ul>';
+                                foreach ($field['value'] as $item) {
+                                    if (isset($item['url'])) {
+                                        echo '<li><img src="' . $item['url'] . '" alt="' . $item['alt'] . '" style="max-width:200px;"/></li>';
+                                    } else {
+                                        echo '<li>' . $item . '</li>';
+                                    }
                                 }
+                                echo '</ul>';
                             }
-                            echo '</ul>';
-                        }
-                        break;
-                    default:
-                        echo $field['value'];
-                        break;
+                            break;
+                        default:
+                            echo $field['value'];
+                            break;
+                    }
                 }
-            }
-                ?></li>
+                ?>
+            </li>
         <?php endforeach; ?>
     </ul>
 <?php endif; ?>
