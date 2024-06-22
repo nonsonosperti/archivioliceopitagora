b28ff89f4f78c6f0f40f82af2263c1a46d61912e
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-22 23:21:49 +0200

    test pagination

--- index.php
+++ index.php
@@ -16,5 +16,6 @@ if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(
 get_template_part( 'entry' );
 comments_template();
 endwhile; endif;
-get_template_part( 'nav', 'below' );
+the_posts_pagination();
+// get_template_part( 'nav', 'below' );
 get_footer();
