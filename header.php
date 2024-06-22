0bc4f4ea9a0358891c8de9501aecb66044561cfb
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-23 01:22:04 +0200

    header mobile

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 <?php wp_body_open(); ?>
 <div id="wrapper" class="hfeed">
 <header>
-<div id="pre-header" class="bg-petrol">
+<div id="pre-header" class="bg-petrol desktop">
     <div class="container">
         <div class="row variable-gutters">
             <div class="col-8">
@@ -23,6 +23,25 @@
                     <strong> <i class="fa fa-phone" aria-hidden="true"></i> 0962905731</strong>
                 </a>
                 <span>Codice Meccanografico KRPC02000L  Codice Fatturazione UFHY7G</span>
+            </div>
+            </div>
+        </div><!-- /row -->
+    </div><!-- /container -->
+</div>
+
+<div id="pre-header" class="bg-petrol mobile">
+    <div class="container">
+        <div class="row variable-gutters">
+            <div class="col-8">
+                <a href="https://www.miur.gov.it/" target="_blank" style="margin-right: 10px" aria-label="MIUR - Collegamento esterno - Apre su nuova scheda">
+                    <strong>MIUR</strong>
+                </a>
+                <a href="mailto:KRPC02000L@istruzione.it" style="margin-right: 10px" target="_blank" aria-label="Indirizzo email del liceo">
+                    <strong> <i class="fa fa-envelope" aria-hidden="true"></i></strong>
+                </a>
+                <a href="tel:00390962905731" style="margin-right: 10px" target="_blank" aria-label="Numero di telefono del liceo">
+                    <strong> <i class="fa fa-phone" aria-hidden="true"></i></strong>
+                </a>
             </div>
             </div>
         </div><!-- /row -->
@@ -62,71 +81,7 @@
                                     </div><!-- /col -->
             </div><!-- /row -->
         </div>
-        <div class="bg-white d-none d-xl-block header-bottom" id="sub-nav">
-            <div class="container">
-                <div class="row variable-gutters">
-                    <div class="col nav-container">
-                        <nav aria-label="Principale" class="main-nav" id="menu-principale">
-                            <ul class="dl-menu nav-list nav-list-primary" data-element="menu">
-                                                                    <li class="text-redbrown menu-dropdown-simple-wrapper">
-                                        <a class="nav-link dropdown-toggle " data-toggle="dropdown" role="button" href="#" aria-expanded="false" id="mainNavDropdown1">Scuola</a>
-                                        <div class="dropdown-menu menu-dropdown dl-submenu menu-dropdown-simple" aria-labelledby="mainNavDropdown1">
-                                            <div class="link-list-wrapper">
-                                                <ul class="link-list" data-element="school-submenu"><li class="menu-title"><div class="h3"><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/la-scuola/" data-element="overview">Panoramica</a></div></li><li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/la-scuola/">La Scuola</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/luogo/" data-element="school-locations">I luoghi</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/scuola/le-persone/">Le persone</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/la-scuola/i-numeri-della-scuola/">I numeri della scuola</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/documento/">Le carte della scuola</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/struttura/">Organizzazione</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/la-scuola/la-storia/">La storia</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/la-scuola/i-nostri-presidi/">I nostri presidi</a></li>
-</ul>                                            </div>
-                                        </div>
-                                    </li>
-                                                                                                        <li class="text-purplelight menu-dropdown-simple-wrapper">
-                                        <a class="nav-link dropdown-toggle " data-toggle="dropdown" role="button" href="#" aria-expanded="false" id="mainNavDropdown2">Servizi</a>
-                                        <div class="dropdown-menu menu-dropdown dl-submenu menu-dropdown-simple" aria-labelledby="mainNavDropdown2">
-                                            <div class="link-list-wrapper">
-                                                <ul class="link-list" data-element="services-submenu"><li class="menu-title"><div class="h3"><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/servizi/" data-element="overview">Panoramica</a></div></li><li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/tipologia-servizio/personale-scolastico/" data-element="service-type">Personale scolastico</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/tipologia-servizio/famiglie-e-studenti/" data-element="service-type">Famiglie e studenti</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/indirizzo-di-studio/">Indirizzi di studio</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/servizio/">Tutti i servizi</a></li>
-</ul>                                            </div>
-                                        </div>
-                                    </li>
-                                                                                                        <li class="text-greendark menu-dropdown-simple-wrapper">
-                                        <a class="nav-link dropdown-toggle " data-toggle="dropdown" role="button" href="#" aria-expanded="false" id="mainNavDropdown3">Novità</a>
-                                        <div class="dropdown-menu menu-dropdown dl-submenu menu-dropdown-simple" aria-labelledby="mainNavDropdown3">
-                                            <div class="link-list-wrapper">
-                                                <ul class="link-list" data-element="news-submenu"><li class="menu-title"><div class="h3"><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/novita/" data-element="overview">Panoramica</a></div></li><li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/tipologia-articolo/notizie/">Notizie</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/tipologia-articolo/articoli/">Articoli</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/circolare/">Circolari</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/evento/">Calendario eventi</a></li>
-<li><a class="list-item" href="https://nuvola.madisoft.it/bacheca-digitale/bacheca/KRPC02000L/1/IN_PUBBLICAZIONE/0/show">Albo Online</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/novita/bacheca-sindacale/">Bacheca Sindacale</a></li>
-</ul>                                            </div>
-                                        </div>
-                                    </li>
-                                                                                                        <li class="text-bluelectric menu-dropdown-simple-wrapper">
-                                        <a class="nav-link dropdown-toggle " data-toggle="dropdown" role="button" href="#" aria-expanded="false" id="mainNavDropdown4">Didattica</a>
-                                        <div class="dropdown-menu menu-dropdown dl-submenu menu-dropdown-simple" aria-labelledby="mainNavDropdown4">
-                                            <div class="link-list-wrapper">
-                                                <ul class="link-list" data-element="teaching-submenu"><li class="menu-title"><div class="h3"><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/didattica/" data-element="overview">Panoramica</a></div></li><li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/didattica/offerta-formativa/">Offerta formativa</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/scheda-didattica/">Le schede didattiche</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/scheda-progetto/">I progetti delle classi</a></li>
-<li><a class="list-item" href="https://www.liceopitagoracrotone.edu.it/tipologia-documento/programmazioni/">Programmazioni</a></li>
-</ul>                                            </div>
-                                        </div>
-                                    </li>
-                                                                </ul><!-- /nav-list -->
-                        </nav>
-	                    <nav aria-label="Argomenti"><ul id="menu-top-1" class="nav-list nav-list-secondary"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2145"><a href="https://www.liceopitagoracrotone.edu.it/prenota-un-appuntamento/">Prenota un Appuntamento</a></li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-2642"><a href="https://www.liceopitagoracrotone.edu.it/archivio/">Archivio</a></li>
-</ul></nav>
-                    </div><!-- /col nav-container -->
-                </div><!-- /row -->
-            </div><!-- /container -->
-        </div>
+        
 </header>
 <div id="container">
 <main id="content" class="container" role="main">
