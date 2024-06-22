88339d06e26c55937071e14d8e6e30f7e54b38fd
nonsonosperti <andrea.sperti@innoversa.it>
2024-06-22 23:55:20 +0200

    test search

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $query = new WP_Query(
         'paged'=> get_query_var( 'paged' )
     ));
 
- echo do_shortcode( '[searchandfilter fields="search, post_types"]' );
+ echo do_shortcode( '[[searchandfilter fields="search,category,post_tag,post_format,albi,albo_pretorio,albo_storico,contenuto_in,plessi_indirizzi,download_tax,portfolio,modulistica_on_line_tax,modulistica_pubblica,modulistica_riservata,pon,responsabile_del_procedimento,tipo_di_evento"]' );
 
 if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post();
 get_template_part( 'entry' );
